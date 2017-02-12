dbfb5d1927176ece7d37df9afa96a2ce586a93f3
Asabeneh <asabeneh@gmail.com>
2017-02-12 19:03:01 +0200

    Delete route added

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ var {User} = require('./models/user');
 var app = express();
 const port = process.env.PORT || 3000;
 
-
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -50,8 +49,26 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send(todo);
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
-  console.log(`Started on port 3000 ${port}`);
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
